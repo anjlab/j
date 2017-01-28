d2167e7e20d7add568a3e23877311488fabb5886
Yury Korolev <yury.korolev@gmail.com>
2017-01-28 20:43:36 +0300

    Port swift JJ to javascript

---
+++ index.js
@@ -0,0 +1,198 @@
+// @flow
+
+import _ from 'lodash'
+
+function _errorMessage(value: any, path:string[], type: string):string {
+  return `Cant't cast '${value}' at ${_.toString(path.join('.'))} to ${type}`
+}
+
+export class JRaw {
+  _v: any;
+  _path: string[]
+
+  constructor(v: any, path:string[]) {
+    this._v = v;
+    this._path = path;
+  }
+
+  isEmpty(): boolean {
+    return _.isEmpty(this._v);
+  }
+
+  raw(): {} {
+    return this._v;
+  }
+}
+
+export class JObj extends JRaw {
+  _v: {}
+  _path: string[]
+
+  get(path: string | string[] | null): JValue {
+    return j(this._v, path, this._path)
+  }
+}
+
+
+export class JValue extends JRaw {
+  _v: any;
+  _path: string[]
+
+  string(): string {
+    if (_.isString(this._v)) {
+      return this._v;
+    }
+    throw new Error(_errorMessage(this._v, this._path, 'string'))
+  }
+
+  stringOrNull(): ?string {
+    if (_.isString(this._v)) {
+      return this._v;
+    }
+    return null;
+  }
+
+  toString(): string {
+    return _.toString(this._v)
+  }
+
+  date(): Date {
+    const v = this.dateOrNull()
+
+    if (v == null) {
+      throw new Error(_errorMessage(this._v, this._path, 'Date'))
+    }
+    return v;
+  }
+
+  dateOrNull(): ?Date {
+    const dateString = this.stringOrNull();
+    const v = Date.parse(dateString || '');
+    if (isNaN(v)) {
+      return null;
+    }
+
+    return new Date(v);
+  }
+
+  integer(): number {
+    if (_.isInteger(this._v)) {
+      return _.toInteger(this._v);
+    }
+
+    throw new Error(_errorMessage(this._v, this._path, 'interger'))
+  }
+
+  integerOrNull(): ?number {
+    if (_.isInteger(this._v)) {
+      return _.toInteger(this._v);
+    }
+
+    return null;
+  }
+
+  toInteger(): number {
+    return _.toInteger(this._v);
+  }
+
+  number(): number {
+    if (_.isNumber(this._v)) {
+      return this._v;
+    }
+
+    throw new Error(_errorMessage(this._v, this._path, 'number'))
+  }
+
+  numberOrNull(): ?number {
+    if (_.isNumber(this._v)) {
+      return this._v;
+    }
+
+    return null;
+  }
+
+  toNumber(): number {
+    return _.toNumber(this._v);
+  }
+
+  bool(): boolean {
+    if (_.isBoolean(this._v)) {
+      return this._v;
+    }
+
+    throw new Error(_errorMessage(this._v, this._path, 'boolean'))
+  }
+
+  boolOrNull(): ?boolean {
+    if (_.isBoolean(this._v)) {
+      return this._v;
+    }
+
+    return null;
+  }
+
+  toBool(): boolean {
+    if (this._v === false || this._v === 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  obj(): JObj {
+    return new JObj(this.object(), this._path);
+  }
+
+  objOrNull(): ?JObj {
+    let v = this.objectOrNull();
+
+    if (v != null) {
+      return new JObj(v, this._path);
+    }
+
+    return null;
+  }
+
+  object(): {} {
+    if (_.isObject(this._v)){
+      return this._v;
+    }
+    throw new Error(_errorMessage(this._v, this._path, 'Object'))
+  }
+
+  objectOrNull(): ?{} {
+    if (_.isObject(this._v)){
+      return this._v;
+    }
+    return null;
+  }
+
+  array(): [] {
+    if (_.isArray(this._v)) {
+      return this._v;
+    }
+
+    throw new Error(_errorMessage(this._v, this._path, 'Array'))
+  }
+
+  arrayOrNull(): ?[] {
+    if (_.isArray(this._v)) {
+      return this._v;
+    }
+
+    return null;
+  }
+}
+
+export default function j(jsonLike: any, path: string | string[] | null = null, parentPath:string[] = ['<root>']): JValue {
+  if (path == null) {
+    return new JValue(jsonLike, parentPath)
+  }
+  if (typeof path == 'string') {
+    path = _.toPath(path);
+  }
+  const v = _.get(jsonLike, path)
+
+  return new JValue(v, parentPath.concat(path));
+}
+
---
+++ index.test.js
@@ -0,0 +1,71 @@
+//@flow
+
+import j from '.'
+
+test('json utitity', () => {
+  let jsonObj = {
+    stringValue: 'yury',
+    boolValue: true,
+    boolFalseValue: false,
+
+    nested: {
+      integer: 1,
+      bool: 1,
+      boolFalse: 0,
+      array: [1, 2, 3],
+      createdAt: '2017-01-21T12:45:55.000Z'
+    }
+  }
+
+  const stringValue = j(jsonObj, 'stringValue').string()
+
+  expect(stringValue).toEqual('yury');
+
+  const boolValue = j(jsonObj, 'boolValue').bool()
+  expect(boolValue).toEqual(true);
+  expect( j(jsonObj, 'boolValue').boolOrNull() ).toBe(true);
+
+  const boolFalseValue = j(jsonObj, 'boolFalseValue').bool()
+  expect(boolFalseValue).toEqual(false);
+
+  const nestedIntValue = j(jsonObj, 'nested.integer').integer()
+  expect(nestedIntValue).toEqual(1);
+
+  const nested = j(jsonObj, 'nested').obj()
+  expect(nestedIntValue).toEqual(1);
+
+  const nestedInteger = nested.get('integer').integer()
+  expect(nestedInteger).toEqual(1)
+
+  const nestedNumber = nested.get('integer').numberOrNull()
+  expect(nestedNumber).toEqual(1)
+
+  const nestedArray = nested.get('array').array()
+  expect(nestedArray).toEqual([1, 2, 3])
+  const nestedArrayOrNull = nested.get('array').arrayOrNull()
+  expect(nestedArrayOrNull).toEqual([1, 2, 3]);
+
+  const nestedDate = nested.get('createdAt').date()
+  expect(nestedDate.getUTCDate()).toEqual(21)
+  expect(nestedDate.getUTCMonth()).toEqual(0)
+  expect(nestedDate.getUTCFullYear()).toEqual(2017)
+  expect(nestedDate.getUTCMinutes()).toEqual(45)
+  expect(nestedDate.getUTCHours()).toEqual(12)
+
+  expect(nested.get('bool').toBool()).toBe(true)
+  expect(nested.get('boolFalse').toBool()).toBe(false)
+  expect(nested.get('bool').boolOrNull()).toBe(null)
+});
+
+test('json utitity errors', () => {
+  let jsonObj = {
+    stringValue: 'yury',
+    boolValue: true,
+    boolFalseValue: false,
+
+    nested: {
+      integer: 1,
+      array: [1, 2, 3]
+    }
+  }
+});
